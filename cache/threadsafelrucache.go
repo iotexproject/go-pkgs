4caf39f269e54af68f07c07d757805e7c5afcf47
millken <millken@gmail.com>
2020-12-24 05:55:45 +0800

    add Range() method for ThreadSafeLruCache (#18)

--- cache/threadsafelrucache.go
+++ cache/threadsafelrucache.go
@@ -7,62 +7,20 @@
 package cache
 
 import (
-	"sync"
-
-	"github.com/golang/groupcache/lru"
+	"github.com/iotexproject/go-pkgs/cache/lru"
 )
 
-// ThreadSafeLruCache defines a lru cache which is thread safe
-type ThreadSafeLruCache struct {
-	cache *lru.Cache
-	mutex sync.RWMutex
-}
+// ThreadSafeLruCache is an alias of lru.Cache
+type ThreadSafeLruCache = lru.Cache
 
 // NewThreadSafeLruCache returns a thread safe lru cache with fix size
 func NewThreadSafeLruCache(maxEntries int) *ThreadSafeLruCache {
-	return &ThreadSafeLruCache{
-		cache: lru.New(maxEntries),
-	}
+	return lru.New(maxEntries)
 }
 
-// Add adds a value to the cache.
-func (c *ThreadSafeLruCache) Add(key lru.Key, value interface{}) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	c.cache.Add(key, value)
-}
-
-// Get looks up a key's value from the cache.
-func (c *ThreadSafeLruCache) Get(key lru.Key) (value interface{}, ok bool) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	return c.cache.Get(key)
-}
-
-// Remove removes the provided key from the cache.
-func (c *ThreadSafeLruCache) Remove(key lru.Key) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	c.cache.Remove(key)
-}
-
-// RemoveOldest removes the oldest item from the cache.
-func (c *ThreadSafeLruCache) RemoveOldest() {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	c.cache.RemoveOldest()
-}
-
-// Len returns the number of items in the cache.
-func (c *ThreadSafeLruCache) Len() int {
-	c.mutex.RLock()
-	defer c.mutex.RUnlock()
-	return c.cache.Len()
-}
-
-// Clear purges all stored items from the cache.
-func (c *ThreadSafeLruCache) Clear() {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	c.cache.Clear()
+// NewThreadSafeLruCacheWithOnEvicted returns a thread safe lru cache with fix size
+func NewThreadSafeLruCacheWithOnEvicted(maxEntries int, onEvicted func(key lru.Key, value interface{})) *ThreadSafeLruCache {
+	cache := lru.New(maxEntries)
+	cache.OnEvicted = onEvicted
+	return cache
 }
